8b7d0093f1b54cf441e7d33c5261d27a1575ab46
chen yan <chenyan@standard-robots.com>
2025-09-30 17:54:35 +0800

    add flags API

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const tasks = require('./tasks/index');
+const flags = require('./flags/index');
 
 // 配置中间件
 app.use(cors()); // 启用跨域支持
@@ -11,6 +12,10 @@ app.use(bodyParser.urlencoded({ extended: true })); // 解析表单数据
 
 app.get('/tasks/:page', tasks.getTasksByPage);
 app.get('/tasks/id/:id', tasks.getTaskById);
+
+// Flags API 路由
+app.get('/flags', flags.getAllFlags);
+app.get('/flags/id/:id', flags.getMilestoneById);
 
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
---
+++ data/milestones.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    id: 1,
+    title: '我的学习里程碑计划',
+    description: '在截止时间前完成学习任务',
+    createdAt: '2021-01-01',
+    tasksId: [3],
+    targetAt: '2022-03-21',
+    mark: '注意查询资料'
+  },
+  {
+    id: 2,
+    title: '我的健康里程碑计划',
+    description: '在截止时间前完成锻炼项目',
+    createdAt: '2021-01-01',
+    tasksId: 2,
+    targetAt: '2022-03-21',
+    mark: '注意查询资料'
+  },
+  {
+    id: 3,
+    title: '我的工作里程碑计划',
+    description: '在截止时间前完成工作任务',
+    createdAt: '2021-01-01',
+    tasksId: [5, 20],
+    targetAt: '2022-03-21',
+    mark: '注意查询资料'
+  },
+  {
+    id: 4,
+    title: '我的生活里程碑计划',
+    description: '在截止时间前完成生活任务',
+    createdAt: '2021-01-01',
+    tasksId: [4, 23],
+    targetAt: '2022-03-21',
+    mark: '注意查询资料'
+  }
+];
